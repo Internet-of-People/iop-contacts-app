a157eff5a8344f5b4aa437f8b62cb7d481854fdb
furszy <mati_fur@hotmail.com>
2017-06-20 20:32:23 -0300

    Starting create profile flow.

--- app/src/main/java/com/example/furszy/contactsapp/CreateProfileActivity.java
+++ app/src/main/java/com/example/furszy/contactsapp/CreateProfileActivity.java
@@ -5,16 +5,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.furszy.contactsapp.ui.home.HomeActivity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Neoperol on 6/20/17.
  */
 
 public class CreateProfileActivity extends BaseActivity {
+    private static final Logger log = LoggerFactory.getLogger(CreateProfileActivity.class);
     private Button buttonCreate;
+    private EditText edit_name;
     @Override
     protected void onCreateView(Bundle savedInstanceState, ViewGroup container) {
         getLayoutInflater().inflate(R.layout.create_profile_activity, container);
+        edit_name = (EditText) findViewById(R.id.edit_name);
         setTitle("Create profile");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -24,9 +34,20 @@ public class CreateProfileActivity extends BaseActivity {
         buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent myIntent = new Intent(v.getContext(), MainActivity.class);
-                startActivityForResult(myIntent, 0);
+                try {
+                    String name = edit_name.getText().toString();
+                    if (name.length() > 0) {
+                        //todo: make this connect non blocking.
+                        anRedtooth.connect(anRedtooth.registerProfile(name));
+                        Intent myIntent = new Intent(v.getContext(), HomeActivity.class);
+                        startActivityForResult(myIntent, 0);
+                    } else
+                        Toast.makeText(v.getContext(), "Please write your profile name", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(v.getContext(), "Registration fail,Please try again later", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                    log.error("Profile registration fail",e);
+                }
             }
         });
 
--- lib_sdk/src/main/java/org/fermat/redtooth/profile_server/ModuleRedtooth.java
+++ lib_sdk/src/main/java/org/fermat/redtooth/profile_server/ModuleRedtooth.java
@@ -24,6 +24,7 @@ public interface ModuleRedtooth {
     void connect(String pubKey) throws Exception;
 
     String registerProfile(String name,String type, byte[] img, int latitude, int longitude, String extraData) throws Exception;
+    String registerProfile(String name) throws Exception;
 
     int updateProfile(String name, ProfSerMsgListener msgListener) throws Exception;
 
