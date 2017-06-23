3b2f24ed3208c4d2d2d46839182c1340f458fd48
Gregory Solarte <Neoperol@Gregorys-MacBook-Pro.local>
2017-06-23 17:55:22 -0400

    Backup view added

--- app/src/main/java/com/example/furszy/contactsapp/ui/settings_backup/SettingsBackupActivity.java
+++ app/src/main/java/com/example/furszy/contactsapp/ui/settings_backup/SettingsBackupActivity.java
@@ -1,32 +1,65 @@
 package com.example.furszy.contactsapp.ui.settings_backup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.furszy.contactsapp.BaseActivity;
 import com.example.furszy.contactsapp.R;
+import com.example.furszy.contactsapp.ui.settings_backup_process.SettingsBackupProcessActivity;
+import com.example.furszy.contactsapp.ui.settings_restore.SettingsRestoreActivity;
 
 /**
  * Created by Neoperol on 6/23/17.
  */
 
 public class SettingsBackupActivity extends BaseActivity {
-
+    Button btnCreateBackup ;
     @Override
     protected void onCreateView(Bundle savedInstanceState, ViewGroup container) {
-        getLayoutInflater().inflate(R.layout.settings_backup_password, container);
+        getLayoutInflater().inflate(R.layout.settings_backup_activity, container);
         setTitle("Backup Profile");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        btnCreateBackup = (Button) findViewById(R.id.createBackup);
+        RadioGroup set_backup = (RadioGroup) findViewById(R.id.toggle);
+        // Toogle button
+        set_backup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // find which radio button is selected
+
+                if(checkedId == R.id.active) {
+                    btnCreateBackup.setVisibility(View.VISIBLE);
+                } else if(checkedId == R.id.inactive) {
+                    btnCreateBackup.setVisibility(View.GONE);
+                }
+            }
+
+        });
+
+
+        // Open Restore
+        btnCreateBackup = (Button) findViewById(R.id.createBackup);
+        btnCreateBackup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), SettingsBackupProcessActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.settings_backup_menu, menu);
-        return true;
-    }
+
+
+
+
 }
