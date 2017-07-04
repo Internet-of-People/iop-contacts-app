1d036d398865d458971f91958f4f3497abfb98b9
Gregory Solarte <Neoperol@Gregorys-MacBook-Pro.local>
2017-07-03 21:26:42 -0400

    Incoming message view added

--- app/src/main/java/com/example/furszy/contactsapp/ui/chat/ChatActivity.java
+++ app/src/main/java/com/example/furszy/contactsapp/ui/chat/ChatActivity.java
@@ -1,8 +1,13 @@
 package com.example.furszy.contactsapp.ui.chat;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.furszy.contactsapp.BaseActivity;
 import com.example.furszy.contactsapp.R;
@@ -19,5 +24,11 @@ public class ChatActivity extends BaseActivity {
     protected void onCreateView(Bundle savedInstanceState, ViewGroup container) {
         super.onCreateView(savedInstanceState, container);
         root = getLayoutInflater().inflate(R.layout.chat_main,container);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#21619C")));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor("#21619C"));
+        }
     }
 }
