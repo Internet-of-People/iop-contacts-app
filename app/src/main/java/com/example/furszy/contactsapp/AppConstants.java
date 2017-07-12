33e352de2aeac7d4f4de0ebfd9c7437a4423d22a
furszy <mati_fur@hotmail.com>
2017-07-12 15:38:45 -0300

    Error report added

---
+++ app/src/main/java/com/example/furszy/contactsapp/AppConstants.java
@@ -0,0 +1,13 @@
+package com.example.furszy.contactsapp;
+
+/**
+ * Created by furszy on 6/28/17.
+ */
+
+public class AppConstants {
+
+    public static final String TEST_PROFILE_SERVER_HOST = "104.197.68.8";
+
+    public static final String REPORT_EMAIL = "matias@furszy.tech";
+    public static final String REPORT_SUBJECT_ISSUE = "report error";
+}
--- app/src/main/java/com/example/furszy/contactsapp/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/furszy/contactsapp/ui/settings/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.furszy.contactsapp.ui.settings;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,11 +13,18 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.furszy.contactsapp.App;
+import com.example.furszy.contactsapp.AppConstants;
 import com.example.furszy.contactsapp.BaseDrawerActivity;
+import com.example.furszy.contactsapp.BuildConfig;
+import com.example.furszy.contactsapp.CrashReporter;
 import com.example.furszy.contactsapp.ProfileActivity;
 import com.example.furszy.contactsapp.R;
+import com.example.furszy.contactsapp.base.ReportIssueDialogBuilder;
 import com.example.furszy.contactsapp.ui.settings_backup.SettingsBackupActivity;
 import com.example.furszy.contactsapp.ui.settings_restore.SettingsRestoreActivity;
+
+import java.io.IOException;
 
 /**
  * Created by Neoperol on 6/21/17.
@@ -25,6 +34,7 @@ public class SettingsActivity  extends BaseDrawerActivity {
     private View root;
     private Button buttonRestore;
     private Button buttonBackup;
+    private Button btn_report;
     private String versionName = "";
     TextView text_version ;
     @Override
@@ -51,6 +61,10 @@ public class SettingsActivity  extends BaseDrawerActivity {
                 app.createProfSerConfig().setBackgroundServiceEnable(isChecked);
             }
         });
+
+        btn_report = (Button) root.findViewById(R.id.btn_report);
+        btn_report.setOnClickListener(this);
+
         // APP Version
         text_version = (TextView) root.findViewById(R.id.text_version);
         try {
@@ -72,12 +86,52 @@ public class SettingsActivity  extends BaseDrawerActivity {
             Toast.makeText(this, "Pairing requests deleted", Toast.LENGTH_LONG).show();
         }else if(id == R.id.btn_backup) {
             Intent myIntent = new Intent(v.getContext(), SettingsBackupActivity.class);
-            startActivityForResult(myIntent, 0);
-        }else if(id == R.id.btn_restore){
+            startActivity(myIntent);
+        }else if(id == R.id.btn_restore) {
             Intent myIntent = new Intent(v.getContext(), SettingsRestoreActivity.class);
-            startActivityForResult(myIntent, 0);
+            startActivity(myIntent);
+        }else if(id == R.id.btn_report){
+            launchReportDialog();
         }else
             super.onClick(v);
+    }
+
+    private void launchReportDialog() {
+        ReportIssueDialogBuilder dialog = new ReportIssueDialogBuilder(
+                this,
+                "com.example.furszy.contactsapp.myfileprovider",
+                R.string.report_issuea_dialog_title,
+                R.string.report_issue_dialog_message_issue)
+        {
+            @Nullable
+            @Override
+            protected CharSequence subject() {
+                return AppConstants.REPORT_SUBJECT_ISSUE+" "+ BuildConfig.VERSION_NAME;
+            }
+
+            @Nullable
+            @Override
+            protected CharSequence collectApplicationInfo() throws IOException {
+                final StringBuilder applicationInfo = new StringBuilder();
+                CrashReporter.appendApplicationInfo(applicationInfo, (App) getApplication());
+                return applicationInfo;
+            }
+
+            @Nullable
+            @Override
+            protected CharSequence collectStackTrace() throws IOException {
+                return null;
+            }
+
+            @Nullable
+            @Override
+            protected CharSequence collectDeviceInfo() throws IOException {
+                final StringBuilder deviceInfo = new StringBuilder();
+                CrashReporter.appendDeviceInfo(deviceInfo, SettingsActivity.this);
+                return deviceInfo;
+            }
+        };
+        dialog.show();
     }
 
     @Override
