57e0bea7a6294a6a4a3317e95b8dbf6c82c4b43e
Wigy <wigy_opensource_developer@yahoo.com>
2017-07-18 14:44:57 +0200

    Trying to fix profile image resizing

--- Android-redtooth/iop_sdk_android/src/main/java/iop/org/iop_sdk_android/core/utils/ImageUtils.java
+++ Android-redtooth/iop_sdk_android/src/main/java/iop/org/iop_sdk_android/core/utils/ImageUtils.java
@@ -5,16 +5,26 @@ import android.graphics.BitmapFactory;
 
 import java.io.ByteArrayOutputStream;
 
-/**
- * Created by furszy on 7/7/17.
- */
-
 public class ImageUtils {
 
-    public static final byte[] compress(byte[] img,int porcentaje){
-        Bitmap bmp = BitmapFactory.decodeByteArray(img,0,img.length);
+    public static final byte[] compressJpeg(byte[] original, int maxLength){
+        Bitmap bmp = BitmapFactory.decodeByteArray(original,0,original.length);
+
+        int quality = 90;
+        byte[] shrunk = toJpeg(bmp, quality);
+        while (shrunk.length > maxLength) {
+            quality -= 10;
+            if (quality < 10) {
+                throw new IllegalArgumentException("Image is too big to fit into the size constraints.");
+            }
+            shrunk = toJpeg(bmp, quality);
+        }
+        return shrunk;
+    }
+
+    private static byte[] toJpeg(Bitmap bmp, int quality) {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 100-porcentaje, bos);
+        bmp.compress(Bitmap.CompressFormat.JPEG, quality, bos);
         return bos.toByteArray();
     }
 
