0ad94877dfefc4fdaf3094f750d3da37adae8438
Yayotron <marsvicam@gmail.com>
2018-01-25 18:43:24 +0100

    - Updated IopLocNet based on the protocol actualization from https://github.com/Internet-of-People/iop-message-protocol/commit/5988a7d63562bdab776bcdadae16dd217ed9ee97 (replaced ServiceType by a string).

--- lib_sdk/src/main/java/org/libertaria/world/locnet/Session.java
+++ lib_sdk/src/main/java/org/libertaria/world/locnet/Session.java
@@ -7,84 +7,81 @@ import java.io.IOException;
 import java.util.List;
 
 
+public class Session {
+    private Connection connection;
 
 
-public class Session
-{
-    Connection connection;
-
-
-    public Session(org.libertaria.world.locnet.NodeInfo node) throws IOException
-    {
-        this( node.getContact().getAddress().getHostAddress(), node.getContact().getPort() );
+    Session(org.libertaria.world.locnet.NodeInfo node) throws IOException {
+        this(node.getContact().getAddress().getHostAddress(), node.getContact().getPort());
 //        System.out.println("Connecting node " + node);
     }
 
-    public Session(String host, int port) throws IOException
-        { connection = new Connection(host, port); }
+    Session(String host, int port) throws IOException {
+        connection = new Connection(host, port);
+    }
 
 
-    public List<org.libertaria.world.locnet.NodeInfo> getNeighbourhood() throws IOException
-    {
+    public List<org.libertaria.world.locnet.NodeInfo> getNeighbourhood() throws IOException {
         IopLocNet.ClientRequest.Builder request = IopLocNet.ClientRequest.newBuilder();
         request.getGetNeighbourNodesBuilder();
 
-        connection.SendRequest( request.build() );
+        connection.SendRequest(request.build());
         IopLocNet.ClientResponse response = connection.ReceiveResponse();
 
-        if ( response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.GETNEIGHBOURNODES )
-            { throw new IllegalStateException("Received unexpected response type"); }
-        return Converter.fromProtoBuf( response.getGetNeighbourNodes().getNodesList() );
+        if (response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.GET_NEIGHBOUR_NODES) {
+            throw new IllegalStateException("Received unexpected response type");
+        }
+        return Converter.fromProtoBuf(response.getGetNeighbourNodes().getNodesList());
     }
 
 
     public List<org.libertaria.world.locnet.NodeInfo> getClosestNodes(GpsLocation location,
-                                                                      float maxRadiusKm, int maxNodeCount, boolean includeNeighbours) throws IOException
-    {
+                                                                      float maxRadiusKm, int maxNodeCount, boolean includeNeighbours) throws IOException {
         IopLocNet.ClientRequest.Builder request = IopLocNet.ClientRequest.newBuilder();
         request.getGetClosestNodesBuilder()
-            .setLocation( Converter.toProtoBuf(location) )
-            .setMaxRadiusKm(maxRadiusKm)
-            .setMaxNodeCount(maxNodeCount)
-            .setIncludeNeighbours(includeNeighbours);
+                .setLocation(Converter.toProtoBuf(location))
+                .setMaxRadiusKm(maxRadiusKm)
+                .setMaxNodeCount(maxNodeCount)
+                .setIncludeNeighbours(includeNeighbours);
 
-        connection.SendRequest( request.build() );
+        connection.SendRequest(request.build());
         IopLocNet.ClientResponse response = connection.ReceiveResponse();
 
-        if ( response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.GETCLOSESTNODES )
-            { throw new IllegalStateException("Received unexpected response type"); }
-        return Converter.fromProtoBuf( response.getGetClosestNodes().getNodesList() );
+        if (response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.GET_CLOSEST_NODES) {
+            throw new IllegalStateException("Received unexpected response type");
+        }
+        return Converter.fromProtoBuf(response.getGetClosestNodes().getNodesList());
     }
 
 
     public List<org.libertaria.world.locnet.NodeInfo> exploreNodes(GpsLocation location,
-                                                                   int targetNodeCount, int maxNodeHops) throws IOException
-    {
+                                                                   int targetNodeCount, int maxNodeHops) throws IOException {
         IopLocNet.ClientRequest.Builder request = IopLocNet.ClientRequest.newBuilder();
         request.getExploreNodesBuilder()
-            .setLocation( Converter.toProtoBuf(location) )
-            .setTargetNodeCount(targetNodeCount)
-            .setMaxNodeHops(maxNodeHops);
+                .setLocation(Converter.toProtoBuf(location))
+                .setTargetNodeCount(targetNodeCount)
+                .setMaxNodeHops(maxNodeHops);
 
-        connection.SendRequest( request.build() );
+        connection.SendRequest(request.build());
         IopLocNet.ClientResponse response = connection.ReceiveResponse();
 
-        if ( response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.EXPLORENODES )
-            { throw new IllegalStateException("Received unexpected response type"); }
-        return Converter.fromProtoBuf( response.getExploreNodes().getClosestNodesList() );
+        if (response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.EXPLORE_NODES) {
+            throw new IllegalStateException("Received unexpected response type");
+        }
+        return Converter.fromProtoBuf(response.getExploreNodes().getClosestNodesList());
     }
 
 
-    public org.libertaria.world.locnet.NodeInfo getNodeInfo() throws IOException
-    {
+    public org.libertaria.world.locnet.NodeInfo getNodeInfo() throws IOException {
         IopLocNet.ClientRequest.Builder request = IopLocNet.ClientRequest.newBuilder();
         request.getGetNodeInfoBuilder();
 
-        connection.SendRequest( request.build() );
+        connection.SendRequest(request.build());
         IopLocNet.ClientResponse response = connection.ReceiveResponse();
 
-        if ( response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.GETNODEINFO )
-            { throw new IllegalStateException("Received unexpected response type"); }
-        return Converter.fromProtoBuf( response.getGetNodeInfo().getNodeInfo() );
+        if (response.getClientResponseTypeCase() != IopLocNet.ClientResponse.ClientResponseTypeCase.GET_NODE_INFO) {
+            throw new IllegalStateException("Received unexpected response type");
+        }
+        return Converter.fromProtoBuf(response.getGetNodeInfo().getNodeInfo());
     }
 }
